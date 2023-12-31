0f2d711e03a911c5c96e57111ae12c1e86d24661
Darian Rodriguez <uceedmr@ucl.ac.uk>
2023-12-31 03:36:42 +0000

    Created CNN Task B

--- B/helper_functions.py
+++ B/helper_functions.py
@@ -5,7 +5,12 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from keras import layers, models
 from sklearn.metrics import classification_report,confusion_matrix
+from sklearn.metrics import precision_recall_fscore_support
 import seaborn as sns
+import pandas as pd
+
+# Set seeds for reproducibility
+tf.random.set_seed(42)
 
 def model_neural_network(params):
 
@@ -77,6 +82,99 @@ def model_nn_regularizer(dropout_value,params):
 
     return historic_data,model
 
+def model_cnn_simple(params):
+
+    """
+    Generate a simple CNN 
+
+    Arguments:
+        params (dict): Dictionary containing parameters for model training. It should contain the necessary arguments
+                     for the `model.fit()` method, such as `x`, `y`, `epochs`, `batch_size`, etc.
+
+    Returns:
+        historic_data: history object from model training
+        model: trained model
+    """
+
+    model = models.Sequential([
+        # Convolutional layers 1
+        layers.BatchNormalization(),
+        layers.Conv2D(64, (2, 2), activation='relu', input_shape=(28, 28, 3)),
+        layers.MaxPooling2D((2, 2)),
+        
+        # Convolutional layers 2
+        layers.Conv2D(64, (2, 2), activation='relu'),
+        layers.MaxPooling2D((2, 2)),
+
+        # Flatten the output of the last convolutional layer
+        layers.Flatten(),
+        
+        # Fully connected layers
+        layers.Dense(512, activation='relu'),
+        
+        # Output layer
+        layers.Dense(9, activation='softmax')  
+    ])
+    
+    # Compile the model
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+    
+    # Train the model 
+    historic_data = model.fit(**params)
+    
+    return historic_data,model
+
+def model_cnn_bn(params):
+    """
+    Generate a CNN  with bath normalization
+
+    Arguments:
+        params (dict): Dictionary containing parameters for model training. It should contain the necessary arguments
+                     for the `model.fit()` method, such as `x`, `y`, `epochs`, `batch_size`, etc.
+
+    Returns:
+        historic_data: history object from model training
+        model: trained model
+    """
+
+    model = models.Sequential([
+        # Convolutional layers with BatchNormalization
+        layers.BatchNormalization(),
+        layers.Conv2D(64, (2, 2), input_shape=(28, 28, 3)),
+        layers.Activation('relu'),
+        layers.MaxPooling2D((2, 2)),
+        
+        
+        layers.Conv2D(64, (2, 2)),
+        layers.BatchNormalization(),
+        layers.Activation('relu'),
+        layers.MaxPooling2D((2, 2)),
+        
+        # Flatten the output of the last convolutional layer
+        layers.Flatten(),
+        
+        # Fully connected layers with BatchNormalization
+        layers.Dense(512),
+        layers.BatchNormalization(),
+        layers.Activation('relu'),
+        
+        # Output layer
+        layers.BatchNormalization(),
+        layers.Dense(9, activation='softmax')  
+    ])
+
+    # Compile the model
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+    
+    # Train the model 
+    historic_data = model.fit(**params)
+    
+    return historic_data,model
+
 def plot_accuracy_epochs(history,name:str):
     """
     Generate plot of the model accuracy across the epochs
@@ -107,17 +205,61 @@ def plot_accuracy_epochs(history,name:str):
     # Save plot in the figures folder
     plt.savefig(f"{folder_path}/{name}.png")
 
-def report_multi_results(y_pred,y_real,labels,name:str):
+
+def report_bar(y_pred,y_true,name:str):
+
     """
-    Generate a classification report and confusion matrix for a multi-class classification problem.
+    Generate F1 Score per class in bar plot
 
     Arguments:
         y_pred (array-like): Predicted labels
         y_real (array-like): True labels
-        labels (list): List of class labels
         name (str): filename
 
     Returns:
+        report_df(dataframe): pandas df with the evaluation metrics
+
+    """
+
+    precision, recall, fscore, support = precision_recall_fscore_support(y_true, y_pred)
+
+    # Create pandas dataframe with the report information
+    report_df = pd.DataFrame({'F1 score': fscore, 'Recall': recall, 'Precision': precision})
+
+    # Create a new figure
+    plt.figure(figsize = (20,8))
+
+    # Create the bar plot
+    bar_container = plt.bar(report_df.index,report_df['F1 score']*100,edgecolor='black', color = sns.color_palette('pastel', 8))
+
+    # Specify the classes
+    plt.xticks(range(0,9))
+
+    # Plot number of each bar
+    plt.bar_label(bar_container, fmt='{:.2f}%',fontsize=12,fontweight='bold')
+
+    plt.title("F1-Score per class",fontsize=18)
+    plt.xlabel('Category', fontsize=14)
+    plt.ylabel('Accuracy%', fontsize=14)
+
+    # Define the path where you want to save the plot
+    folder_path = "./B/figures"
+
+    # Save plot in the figures folder
+    plt.savefig(f"{folder_path}/{name}.png")
+
+    return report_df
+
+def report_multi_results(y_pred,y_real,labels,path):
+
+    """
+    Generate report for the Classifier.
+
+    Args:
+        y_pred (array-like): Predicted labels generated by the classifier.
+        y_real (array-like): Ground truth (real) labels.
+        labels(str): xticks labels
+        path (str) : Path or file location to report the results.
     """
 
     # Generate classification report
@@ -125,12 +267,8 @@ def report_multi_results(y_pred,y_real,labels,name:str):
     print("Classification Report:")
     print(report)
 
-    # Confusion Matrix
-    print("Confusion Matrix:")
-
     # Calculate confusion matrix
     conf_matrix = confusion_matrix(y_real, y_pred)
-    print(confusion_matrix)
 
     # Create a heatmap for the confusion matrix
     plt.figure(figsize=(6, 4))
@@ -140,9 +278,6 @@ def report_multi_results(y_pred,y_real,labels,name:str):
     plt.title('Confusion Matrix')
     plt.xlabel('Predicted label')
     plt.ylabel('True label')
-   
-    # Define the path where you want to save the plot
-    folder_path = "./B/figures"
 
-    # Save plot in the figures folder
-    plt.savefig(f"{folder_path}/{name}.png")
+    # Save plot in the path
+    plt.savefig(f"{path}.png")
--- B/task.py
+++ B/task.py
@@ -62,5 +62,50 @@ def multiclass_task():
     # Plot Report regularized NN
     hf.report_multi_results(y_pred,pathmnist.y_val,class_labels,"Confusion matrix regularized nn")
 
-    
+    # Create instance of the simple CNN
+    historic_cnn,model_cnn = hf.model_cnn_simple(params)
+
+    # Create instance of the CNN with batch normalization
+    historic_cnn_bn,model_cnn_bn = hf.model_cnn_bn(params)
+
+    # Plot model performance during epochs
+    hf.plot_accuracy_epochs(historic_cnn,"Epochs Accuracy simple CNN")
+    hf.plot_accuracy_epochs(historic_cnn_bn,"Epochs Accuracy CNN BN")
+
+    # Prediction from the validation set for the simple CNN
+    val_predictions = model_cnn.predict(pathmnist.x_val)
+    y_pred = tf.argmax(val_predictions, axis=1)
+
+    # Plot Report simple CNN
+    hf.report_multi_results(y_pred,pathmnist.y_val,class_labels,"Confusion matrix CNN BN")
+
+    # Prediction from the validation set for the CNN Batch normalization
+    val_predictions = model_cnn_bn.predict(pathmnist.x_val)
+    y_pred = tf.argmax(val_predictions, axis=1)
+
+    # Plot Report CNN Batch normalization
+    hf.report_multi_results(y_pred,pathmnist.y_val,class_labels,"Confusion matrix CNN BN")
+
+    # Report ROC Curve on the testing set for the different propose models
+    models = [model_nn,model_reg,model_cnn,model_cnn_bn]
+    labels = ["NN","NN regularizer","CNN","CNN normalization"]
+    class_labels = np.unique(pathmnist.y_train)
+
+    for index,model in enumerate(models):
+
+        # Prediction from test set
+        test_predictions = model.predict(pathmnist.x_test)
+        y_pred = tf.argmax(test_predictions, axis=1)
+
+        # Plots to save on test set
+        chart = hf.report_bar(pathmnist.y_test,y_pred,f"F1 score {labels[index]} TS")
+        hf.report_multi_results(y_pred,pathmnist.y_test,class_labels,f"./B/figures/{labels[index]} Confusion matrix TS")
+
+
+
+
+
+
+
+
 
